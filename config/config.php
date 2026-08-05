fb9415538427e585904f7c27c5981ec5c56455eb
MarkOLockwood <vipsoc.redin@yandex.ru>
2026-08-05 16:59:49 +0300

    update config

---
+++ config/config.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => '5432',
+        'dbname' => 'mytwit',
+        'username' => 'mytwit',
+        'password' => 'mytwit',
+        'charset' => 'utf8'
+    ],
+    'jwt' => [
+        'secret' => 'your_jwt_secret_key_change_this_in_production',
+        'expiration' => 86400 * 7 // 7 дней
+    ],
+    'app' => [
+        'url' => 'http://mytwit.com',
+        'api_prefix' => '/api',
+        'environment' => 'development' // development или production
+    ],
+    'ffmpeg' => [
+        'binary' => '/usr/bin/ffmpeg',
+        'ffprobe' => '/usr/bin/ffprobe'
+    ]
+];
